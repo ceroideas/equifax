38ae6c54e76066bd3f7ab7361aa8f72d223bb2fb
Grimsley <grimsleyrojas@gmail.com>
2022-03-16 20:41:21 -0400

    Inicio de la creación de reclamos

---
+++ database/migrations/2022_03_16_220628_create_claims_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('claims', function (Blueprint $table) {
+            $table->id();
+            $table->integer('status');
+            $table->boolean('claim_type');
+            $table->foreignid('third_parties_id');
+            $table->foreignid('debts_id');
+            $table->foreignid('debtors_id');
+            $table->foreignid('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('claims');
+    }
+};
---
+++ resources/views/claims/create.blade.php
@@ -0,0 +1,47 @@
+@extends('adminlte::page')
+
+@section('title', 'Crear Usuario')
+
+@section('content_header')
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Crear Nueva Reclamación</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="/panel">Dashboard</a></li>
+                    <li class="breadcrumb-item"><a href="/claims">Reclamaciones</a></li>
+                    <li class="breadcrumb-item active">Nueva Reclamación</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('content')
+   {{-- @include('users.partials._form') --}}
+   <x-adminlte-card header-class="text-center" theme="orange" theme-mode="outline">
+      <div class="row">
+        <div class="col-sm-12 text-center">
+            <span> <h1>¿Reclama en nombre del usuario registrado?</h1></span>    
+        </div>          
+      </div>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+            <span> <button class="btn btn-flat btn-success question-button" href="{{ url('claims/create/step-two') }}">SI</button></span>    
+            <span> <button class="btn btn-flat btn-danger  question-button" >NO</button></span> 
+        </div>          
+      </div>
+   </x-adminlte-card>
+@stop
+
+@section('js')
+<script>
+    
+   $('.question-button').on('click', function(){
+       console.log($(this).attr('href'));
+        location.href = $(this).attr('href');
+   });
+</script>
+@stop
