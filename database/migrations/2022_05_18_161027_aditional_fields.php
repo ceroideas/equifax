0d98b464db5f41cd63574d03d3d0562740fcc111
Jorge Solano <jorgesolano92@gmail.com>
2022-06-05 09:58:58 -0400

    ultimos cambios

--- database/migrations/2022_05_18_161027_aditional_fields.php
+++ database/migrations/2022_05_18_161027_aditional_fields.php
@@ -29,13 +29,40 @@ return new class extends Migration
             $table->string('apud_acta')->nullable();
             $table->integer('newsletter')->nullable();
         });
+
+        Schema::table('claims', function(Blueprint $table) {
+            //
+            $table->string('phase')->nullable();
+        });
+        Schema::table('actuations', function(Blueprint $table) {
+            //
+            $table->integer('hito_padre')->nullable();
+        });
+
+        Schema::create('debt_documents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('debt_id')->nullable();
+            $table->string('document')->nullable();
+            $table->string('type')->nullable();
+            $table->text('hitos')->nullable();
+            $table->timestamps();
+            //
+        });
+
+        Schema::table('debts', function(Blueprint $table) {
+            //
+            $table->integer('reclamacion_previa_indicar')->nullable();
+            $table->string('fecha_reclamacion_previa')->nullable(); // documento que acredite dicha reclamacion
+
+            $table->text('partials_amount_details')->nullable(); // detalle de pagos y fechas de los pagos
+        });
     }
 
     /**
      * Reverse the migrations.
      *
      * @return void
-     */
+     
     public function down()
     {
         //
