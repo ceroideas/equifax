072637391174faf862288106f367dd855ed465af
Luis Campos <lcamposg@primor.eu>
2022-08-25 14:49:25 +0200

    Cambio diseño textos home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -381,9 +381,8 @@
                         <div data-v-9cc878a2="" class="text-tarifa" style="padding:0">¿Cu&aacute;l es nuestra <b>misi&oacute;n</b>?</div>
                         <div class="container text-center bottom-text" style="font-weight:normal !important">
                             <p>En <b>Dividae</b>, queremos revolucionar el mundo de la recuperaci&oacute;n de facturas impagadas a nivel digital de una manera
-                                <b>automatizada</b> y <b>transparente</b>.</p>
-                            <p>¿Nuestro principal objetivo? Que digas <b>adi&oacute;s a tus facturas impagadas</b>.</p>
-                            <p>Conoce m&aacute;s acerca de <b><a href="{{ url('quienes-somos') }}">nosotros</a></b>.</p>
+                                <b>automatizada</b> y <b>transparente</b>. ¿Nuestro principal objetivo? Que digas <b>adi&oacute;s a tus facturas impagadas</b>.Conoce m&aacute;s acerca de 
+                                <b><a href="{{ url('quienes-somos') }}">nosotros</a></b>.</p>
                         </div>
 
                         <div data-v-63cd6604="" data-v-effc9f78=""><a data-v-63cd6604="" href="{{ url('register') }}" class="btn QSomos-btn" data-v-effc9f78="">Regístrate</a></div>
