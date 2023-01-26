d34b5b7bea57a166d1be7e2b0e15aee7786fda34
asemsqa <asesqa@gmail.com>
2023-01-26 15:15:47 +0100

    Quitar ejemplo de paginacion adicional

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -67,30 +67,14 @@
                                 </div>
 
                             @endforeach
-                            {{-- Ejemplo paginacion bootstrap
-                            <nav aria-label="Page navigation example">
-                                <ul class="pagination">
-                                  <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                                  <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                  <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                  <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                  <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                                </ul>
-                            </nav>--}}
-
                         </div>
                         <div>{{ $blogs->links() }}</div>
                     </div>
-
                 </div>
-                {{ $blogs->onEachSide(4)->links() }}
-
                 @include('footer')
-
             </div>
         </main>
     </div>
-    <!-- Scripts -->
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
