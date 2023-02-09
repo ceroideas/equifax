a2f190a8ed4baf872a311eb4cf10f82e27a6c9df
asemsqa <asesqa@gmail.com>
2023-02-09 12:13:32 +0100

    Limipieza comentarios y default selected value

--- resources/views/claims/actuations.blade.php
+++ resources/views/claims/actuations.blade.php
@@ -41,7 +41,6 @@
                 </div>
             </div>
             <x-slot name="footerSlot">
-                {{-- <x-adminlte-button class="mr-auto" theme="success" label="Accept"/> --}}
                 <x-adminlte-button theme="danger" label="Cerrar" data-dismiss="modal"/>
             </x-slot>
         </x-adminlte-modal>
@@ -58,10 +57,8 @@
         </div>
 
         <div class="card-body">
-
         	<div class="row">
         		<div class="col-md-12">
-
         			<div class="row text-left">
         			@forelse ($actuations as $act)
 
@@ -71,159 +68,79 @@
                                 {{$act->subject}}</b> ( {!! date('d/m/Y', strtotime($act->actuation_date)) !!} )</h6>
 
         					<p style="margin: 0">{{$act->description}}</p>
-
-        					{{-- <p>
-        						<b>Resultado de la actuación:</b> {!! $act->type ? '<span class="text-success">Exitoso</span>' : '<span class="text-warning">No exitoso</span>' !!}
-        					</p> --}}
-        					{{-- <p>
-        						<b>Fecha de la actuación:</b>
-        					</p> --}}
-
         					@if ($act->amount)
         						<b style="color: #333">Importe recuperado:</b> {{ number_format($act->amount, 2,',','.') }} € <br>
-
-        						{{-- @if ($act->invoice)
-        						<b>Importe a facturar:</b> {{$act->invoice->amount}}€ <br>
-        						<b>Status de la factura:</b> {!!$act->invoice->status ? '<span class="text-success">Pagado</span>' : '<span class="text-info">Pendiente</span>'!!}
-        						@else
-        							<i>No se ha generado una factura</i>
-        						@endif --}}
         					@endif
 
         					@if ($act->documents)
-
         						@foreach ($act->documents as $document)
-
         						<div class="row">
-
         							<div class="col-sm-4">
-
         								<li>
                                         	<a href="{{ url('/uploads/actuations/' . $act->id . '/documents',$document->document_name) }}" class="btn-link text-secondary" target="_blank" download="{{$document->document_name}}"><i class="far fa-fw fa-file"></i>{{$document->document_name}}</a>
                                     	</li>
-
         							</div>
-
         						</div>
-
         						@endforeach
-
         					@endif
-
         					<hr>
-
         				</div>
-
         			@empty
-
-        			<div class="col-sm-12">
-        				@if ($claim->isFinished())
-        				<h4>No se registraron actuaciones en esta reclamación</h4>
-        				@else
-        				<h4>No se han registrado actuaciones en esta reclamación</h4>
-        				@endif
-        			</div>
-
+                        <div class="col-sm-12">
+                            @if ($claim->isFinished())
+                            <h4>No se registraron actuaciones en esta reclamación</h4>
+                            @else
+                            <h4>No se han registrado actuaciones en esta reclamación</h4>
+                            @endif
+                        </div>
         			@endforelse
         			</div>
-
         		</div>
         	</div>
 
-            {{--@if ((Auth::user()->isSuperAdmin() || Auth::user()->isAdmin() || Auth::user()->isGestor()) && !$claim->isFinished())--}}
             @if ((Auth::user()->isSuperAdmin() || Auth::user()->isAdmin()) && !$claim->isFinished())
             	<hr>
-
-
             	<form action="{{url('claims/actuations',$claim->id)}}" method="POST" id="actuation-form" enctype="multipart/form-data">
             		{{csrf_field()}}
 
 	            	<h4>Registrar nueva actuación</h4>
 
 	            	<div class="row">
-
-	            		{{-- <div class="col-sm-6">
-
-	            			<div class="form-group">
-
-	            				<label for="phase">Fase</label>
-
-	            				@php
-	            					$config = ['tags' => true];
-	            				@endphp
-
-	            				<x-adminlte-select2 name="phase" id="phase" class="form-control select2" :config="$config" required>
-
-	            					<option disabled=""></option>
-	            					@foreach (config('app.phases') as $key => $ph)
-
-	            						<option {{isset($claim->phase) ? ($claim->phase == $key ? 'selected' : '') : ''}} value="{{$key}}">{{$ph}}</option>
-
-	            					@endforeach
-	            				</x-adminlte-select2>
-
-	            			</div>
-
-	            		</div> --}}
-
 	            		<div class="col-sm-6">
-
 	            			<div class="form-group">
-
 	            				<label for="subject">Hito</label>
-
 	            				<div id="select-ph">
 	            					@include('claims.hitos', ['phase' => $claim->getPhase()])
 	            				</div>
 	            			</div>
-
 	            		</div>
 	            		<div class="col-sm-6">
-
 	            			<div class="form-group">
-
 	            				<label for="actuation_date">Fecha de la actuación</label>
-
 	            				@php
 									$config = ['format' => 'DD-MM-YYYY'];
-                                    //$valor= [date("d-m-Y")]
 								@endphp
-
 	            				<x-adminlte-input-date name="actuation_date" id="actuation_date" :config="$config" required/>
-
 	            			</div>
 	            		</div>
 
 	            		<div class="col-sm-12">
-
 	            			<div class="form-group">
-
 	            				<label for="description">Descripción</label>
-
-
 	            				<x-adminlte-textarea name="description" id="description" placeholder="Inserte descripción..."/>
-
-
 	            			</div>
-
 	            		</div>
 
 	            		<div class="col-sm-12">
-
 	            			<div class="form-group">
-
 	            				<x-adminlte-input-file id="ifMultiple" name="files[]" label="Archivos de la actuacion"
 								    placeholder="Puede subir varios archivos..." igroup-size="lg" legend="Seleccione" multiple>
-								    {{-- <x-slot name="appendSlot">
-								        <x-adminlte-button theme="primary" label="Upload"/>
-								    </x-slot> --}}
 								    <x-slot name="prependSlot">
 								        <div class="input-group-text text-primary">
 								            <i class="fas fa-file-upload"></i>
 								        </div>
 								    </x-slot>
 								</x-adminlte-input-file>
-
 	            			</div>
 	            		</div>
 	            		<div class="col-sm-12 d-none">
@@ -231,12 +148,9 @@
 	            			<x-adminlte-input-switch name="type" id="type" data-on-text="Exitoso" data-off-text="No Exitoso" data-on-color="teal" checked/>
 	            		</div>
 	            	</div>
-
 	            	<hr>
-
 	            	<div class="row" style="display: none-;">
 	            		<div class="col-sm-12">
-
 	            			<x-adminlte-input name="amount" label="Si se ha recuperado algún importe, especificarlo" placeholder="Importe" min="0" step="0.01" type="number"
 			                    igroup-size="sm" >
 			                        <x-slot name="appendSlot">
@@ -245,10 +159,8 @@
 			                            </div>
 			                        </x-slot>
 			                </x-adminlte-input>
-
 	            		</div>
 	            	</div>
-
 
 	            	<div class="d-none">
 		            	<div class="row" style="display: none-;" id="invoice-data">
@@ -257,79 +169,39 @@
 		            			<x-adminlte-input-switch name="mailable" id="mailable" data-on-text="Si" data-off-text="No" data-on-color="teal"/>
 		            		</div>
 		            	</div>
-
 		            	<hr>
-
 		            	<div id="invoice-data">
-
-			            	{{-- <div class="row" style="display: none-;">
-			            		<div class="col-sm-12">
-
-			            			<x-adminlte-input name="amount" label="Si se ha recuperado algún importe, especificarlo" placeholder="Importe" step="0.01" type="number"
-					                    igroup-size="sm" >
-					                        <x-slot name="appendSlot">
-					                            <div class="input-group-text bg-dark">
-					                                <i class="fas fa-euro"></i>
-					                            </div>
-					                        </x-slot>
-					                </x-adminlte-input>
-
-			            		</div>
-			            	</div> --}}
-
 			            	<div class="row">
 			            		<div class="col-sm-12">
 			            			<label for="invoice">¿Desea generar factura por el importe recuperado? (ésto en caso que el deudor haya realizado la transferencia al cliente en vez de a DIVIDAE)</label>
 			            			<x-adminlte-input-switch name="invoice" data-on-text="Si" data-off-text="No" data-on-color="teal"/>
 			            		</div>
 			            	</div>
-
 			            	<hr>
-
 		            	</div>
 
 		            	<div id="honorarios-data">
-
 			            	<div class="row">
 			            		<div class="col-sm-12">
 			            			<label for="invoice_2">¿Desea generar factura por el pago de honorarios adicionales?</label>
 			            			<x-adminlte-input-switch name="invoice_2" data-on-text="Si" data-off-text="No" data-on-color="teal"/>
 			            		</div>
 			            	</div>
-
 			            	<div class="row" style="display: none;" id="invoice-data-2">
 			            		<div class="col-sm-12">
-
 			            			<x-adminlte-input name="honorarios" label="Si se requiere un pago adicional, especificar" placeholder="Importe" step="0.01"  min="0"
 					                    igroup-size="sm" >
-					                        {{-- <x-slot name="appendSlot">
-					                            <div class="input-group-text bg-dark">
-					                                <i class="fas fa-euro"></i>
-					                            </div>
-					                        </x-slot> --}}
 					                </x-adminlte-input>
-
 					                <x-adminlte-input name="concepto" label="Concepto de la factura" placeholder="Concepto" type="text"
 					                    igroup-size="sm" >
-					                        {{-- <x-slot name="appendSlot">
-					                            <div class="input-group-text bg-dark">
-					                                <i class="fas fa-euro"></i>
-					                            </div>
-					                        </x-slot> --}}
 					                </x-adminlte-input>
-
 			            		</div>
 			            	</div>
 		            	</div>
 	            	</div>
-
-
-
 	            	<x-adminlte-button class="btn-flat btn-sm float-right" type="submit" label="Guardar" theme="success" icon="fas fa-lg fa-save"/>
-
             	</form>
             @endif
-
         </div>
     </div>
 
@@ -392,15 +264,5 @@
 				console.log("complete");
 			});
 		});
-
-		/*$('#phase').change(function (e) {
-			e.preventDefault();
-
-			$.get('{{url('loadActuations')}}/'+$(this).val(), function(data) {
-				$('#select-ph').html(data);
-			});
-		});*/
-
-		// $('#phase').trigger('change');
 	</script>
 @stop
--- resources/views/claims/hitos.blade.php
+++ resources/views/claims/hitos.blade.php
@@ -4,21 +4,18 @@ $config = ['tags' => true];
 
 <x-adminlte-select2 name="subject" id="subject" class="form-control select2" :config="$config" required>
 
-	{{-- @foreach (config('app.actuations') as $key => $ph) --}}
 	@foreach (App\Models\Hito::whereNull('parent_id')->get() as $key => $ph)
-
 		@if (count($ph['hitos']) && in_array($phase[0], $ph['phase']) && $ph['name'])
 			@if ($ph['hitos'])
-			<optgroup label="{{$ph['name']}}">
-				@foreach ($ph['hitos'] as $ht)
-			    	<option value="{{$ht['ref_id']}}">{{$ht['ref_id']}} - {{$ht['name']}}</option>
-				@endforeach
-			</optgroup>
+                <optgroup label="{{$ph['name']}}">
+                    @foreach ($ph['hitos'] as $ht)
+                        <option value="{{$ht['ref_id']}}" @if($ht['ref_id']==30002) selected @endif>{{$ht['ref_id']}} - {{$ht['name']}}</option>
+                    @endforeach
+                </optgroup>
 			@else
 				<option value="{{$ph['ref_id']}}">{{$ph['ref_id']}} - <b>{{$ph['name']}}</b></option>
 			@endif
 		@endif
-
 	@endforeach
 
 </x-adminlte-select2>
