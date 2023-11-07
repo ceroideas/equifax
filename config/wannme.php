73845d4f3c6ad2b50f1f41afc73cb0770b9eeee0
asemsqa <asesqa@gmail.com>
2023-11-07 14:06:41 +0100

    Change authorization wannme code

--- config/wannme.php
+++ config/wannme.php
@@ -1,15 +1,15 @@
 <?php
 
 
-if($_SERVER['SERVER_NAME']=='develop.dividae.com'||$_SERVER['SERVER_NAME']=='127.0.0.1'){
+/*if($_SERVER['SERVER_NAME']=='develop.dividae.com'||$_SERVER['SERVER_NAME']=='127.0.0.1'){*/
     /* Entorno test 1.autorization, 2.url, 3.partner_id, 4.private_key */
-    return [
+    /*return [
         'arg1'=> '778199111ac61968b18ac08c36aa04b44aa0ecc6',
         'arg2'=> 'https://rest-demo.wannme.com:443/integration/v2/wannmepay/payment/',
         'arg3'=> '-dq7jmsf5v3i6oiockve',
         'arg4'=>'AXLQtVw*9pgJ541PUdRy8oGVKQIxS5'
         ];
-}else{
+}else{*/
 
     /* Claves de entorno produccion */
     return [
@@ -18,4 +18,4 @@ if($_SERVER['SERVER_NAME']=='develop.dividae.com'||$_SERVER['SERVER_NAME']=='127
         'arg3'=> 'pwfnuv4yqxze9z-ok52-',
         'arg4'=>'qlQrVtXyqzuJBTPlFCe0EKHt8310s5'
         ];
-}
+/*}*/
