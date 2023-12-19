eadd7e2eff603f5ca6a98c6b584500dfa6cbcf75
asemsqa <asesqa@gmail.com>
2023-12-19 14:54:57 +0100

    Add callback route

--- config/wannme.php
+++ config/wannme.php
@@ -7,7 +7,10 @@ if($_SERVER['SERVER_NAME']=='develop.dividae.com'||$_SERVER['SERVER_NAME']=='127
         'arg1'=> '778199111ac61968b18ac08c36aa04b44aa0ecc6',
         'arg2'=> 'https://rest-demo.wannme.com:443/integration/v2/wannmepay/payment/',
         'arg3'=> '-dq7jmsf5v3i6oiockve',
-        'arg4'=>'AXLQtVw*9pgJ541PUdRy8oGVKQIxS5'
+        'arg4'=>'AXLQtVw*9pgJ541PUdRy8oGVKQIxS5',
+        'notificationURL'=> 'https://develop.dividae.com/callback',
+        'returnOKURL'=> 'https://develop.dividae.com/callback',
+        'returnKOURL'=> 'https://develop.dividae.com/callback'
         ];
 }else{
 
@@ -16,6 +19,9 @@ if($_SERVER['SERVER_NAME']=='develop.dividae.com'||$_SERVER['SERVER_NAME']=='127
         'arg1'=> 'eyJhbGciOiJIUzUxMiJ9.eyJxcsOTIjoiY20yMTJiMTExdiIsInRnd2XDiSI6Ikc1U1BDQUtTR0g1Iiwiw4lraHHDjXdresOJa8OTIjoidXJzdMOJIiwiZWE4d3lxaXdKIGvDk3c3w4lrw5NyIjoiY2LDsXYiLCJnd3nDiSI6IjlqTWZudmU3V2x6d3Z6eFFpM0M3RFE9PSIsInQgaiI6ImNiw7F2IiwicXkiOiJidjEiLCJhw4nDmjVyesOJIjoiWnFocXlyw4nCo2XDjXd5In0._O445pXEEME_tdLotAAtZLH_Ds_B_TSNj_HluEZB1lmA3a-r12MNlzotMoOtctuTD__Hvf_YOO1lZYCzJKhdwg',
         'arg2'=> 'https://rest.wannme.com/integration/v2/wannmepay/payment/',
         'arg3'=> 'pwfnuv4yqxze9z-ok52-',
-        'arg4'=>'qlQrVtXyqzuJBTPlFCe0EKHt8310s5'
+        'arg4'=>'qlQrVtXyqzuJBTPlFCe0EKHt8310s5',
+        'notificationURL'=> 'https://dividae.com/callback',
+        'returnOKURL'=> 'https://dividae.com/callback',
+        'returnKOURL'=> 'https://dividae.com/callback'
         ];
 }
