8efd9b6a00e6fbc363e136e737dfc027523ac0bb
asemsqa <asesqa@gmail.com>
2024-03-04 16:20:48 +0100

    Add captcha in register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,7 @@ class RegisterController extends Controller
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'tos' => ['required'],
             'lopd' => ['required'],
+            'g-recaptcha-response' => 'required|captcha',
             /* 'g-recaptcha-response' => 'required|recaptchav3:register,0.5' */
         ]);
     }
