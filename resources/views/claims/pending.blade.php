5d147a3935a3fe9535c262c34ab5171b3371cb82
asemsqa <asesqa@gmail.com>
2024-03-26 17:12:58 +0100

    Fix pending claims grid

--- resources/views/claims/pending.blade.php
+++ resources/views/claims/pending.blade.php
@@ -96,9 +96,9 @@
 
                     <td>{{ $decryptedName }}</td>
                     <td>{{ $decryptedDebtorName }}</td>
-                    <td>{{ number_format($claim->debt->pending_amount, 2,',','.') }} &euro;</td>
+                    <td>{{ isset($claim->debt) ? number_format($claim->debt->pending_amount, 2,',','.') : '-'  }} &euro;</td>
                     <td>{{ number_format($claim->amountClaimed(), 2,',','.') /* + $claim->debt->partialAmounts()*/ }} &euro;</td>
-                    <td>{{ number_format($claim->debt->pending_amount - ($claim->amountClaimed()/* + $claim->debt->partialAmounts()*/), 2,',','.') }} &euro;</td>
+                    <td>{{ isset($claim->debt) ? number_format($claim->debt->pending_amount - ($claim->amountClaimed()/* + $claim->debt->partialAmounts()*/), 2,',','.') :'-'}} &euro;</td>
                     <td>{{ $claim->getType() }}</td>
                     <td>{{ $claim->getHito() }}</td>
                     <td>
@@ -131,5 +131,4 @@
             @endforeach
         </x-adminlte-datatable>
     </x-adminlte-card>
-
 @stop
